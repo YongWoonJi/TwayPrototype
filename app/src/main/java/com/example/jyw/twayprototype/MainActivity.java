a6694b6bd3f127e25f744a82950dda660cf48a1b
YongWoonJi <woon013@naver.com>
2016-08-18 20:47:57 +0900

    8. 18

--- app/src/main/java/com/example/jyw/twayprototype/MainActivity.java
+++ app/src/main/java/com/example/jyw/twayprototype/MainActivity.java
@@ -9,10 +9,18 @@ import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
     CollapsingToolbarLayout collapsing_toolbar;
     AppBarLayout appbar;
     ImageView imageView;
+    ImageView rightIcon;
+
+    private static final String TAB1 = "tab1";
+    private static final String TAB2 = "tab2";
+    private static final String TAB3 = "tab3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         appbar = (AppBarLayout) findViewById(R.id.appbar);
-        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView = (ImageView) findViewById(R.id.logo);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -38,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
         final ColorDrawable newColor = new ColorDrawable(getResources().getColor(android.R.color.white));
         final Drawable homeAsUp = getResources().getDrawable(android.R.drawable.ic_dialog_info);
+//        final Drawable rightIcon = ContextCompat.getDrawable(this, android.R.drawable.ic_dialog_alert);
         newColor.setAlpha(0);
         appbar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
             @TargetApi(Build.VERSION_CODES.LOLLIPOP)
@@ -45,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
             public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
                 float offsetAlpha = (appBarLayout.getY() / appbar.getTotalScrollRange());
 //                imageView.setAlpha( 1 - (offsetAlpha * -1));
-                Log.i("AAAAA", "offsetAlpha값: " + 150 * offsetAlpha);
+                Log.i("AAAAA", "offsetAlpha값: " + (-150 * offsetAlpha) + " , offset: " + offsetAlpha);
                 Log.i("AAAAA", "값: " + (1 - (offsetAlpha * -1)) * 255);
 
 
@@ -57,14 +71,22 @@ public class MainActivity extends AppCompatActivity {
 //                homeAsUp.setColorFilter(cf);
                 int value;
                 if (((int)(150 * offsetAlpha)) == 0) {
-                    value = -6;
+                    value = 255;
                 } else {
-                    value = (int) (-150 * offsetAlpha);
+                    value = (int) (255 - (-150 * offsetAlpha));
                 }
 //                homeAsUp.setColorFilter(Color.rgb(value, value, value), PorterDuff.Mode.SRC_IN);
                 homeAsUp.setTint(Color.rgb(value, value, value));
 
+                imageView.setColorFilter(Color.rgb(255, value, value));
+
+                if (rightIcon != null) {
+                    rightIcon.setColorFilter(Color.rgb(value, value, value));
+                }
+
                 getSupportActionBar().setHomeAsUpIndicator(homeAsUp);
+
+
             }
         });
 
@@ -77,8 +99,58 @@ public class MainActivity extends AppCompatActivity {
 
 
         tabs = (TabLayout) findViewById(R.id.tabs);
-        tabs.addTab(tabs.newTab().setIcon(android.R.drawable.ic_media_play).setText("항공권 예매").setTag("tab1"));
-        tabs.addTab(tabs.newTab().setIcon(android.R.drawable.ic_dialog_alert).setText("이벤트").setTag("tab2"));
-        tabs.addTab(tabs.newTab().setIcon(android.R.drawable.ic_dialog_map).setText("운항 스케쥴").setTag("tab3"));
+        tabs.addTab(tabs.newTab().setIcon(android.R.drawable.ic_media_play).setText("항공권 예매").setTag(TAB1));
+        tabs.addTab(tabs.newTab().setIcon(android.R.drawable.ic_dialog_alert).setText("이벤트").setTag(TAB2));
+        tabs.addTab(tabs.newTab().setIcon(android.R.drawable.ic_dialog_map).setText("운항 스케쥴").setTag(TAB3));
+        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getPosition()) {
+                    case 0:
+                        Toast.makeText(MainActivity.this, "tab1", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        Toast.makeText(MainActivity.this, "tab2", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+                        Toast.makeText(MainActivity.this, "tab3", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+        try {
+            Field declaredFiled = toolbar.getClass().getDeclaredField("mTitleTextView");
+            declaredFiled.setAccessible(true);
+            ImageView logoView = (ImageView) declaredFiled.get(toolbar);
+            ViewGroup.LayoutParams layoutParams = logoView.getLayoutParams();
+            layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem item = menu.findItem(R.id.item1);
+        View view = MenuItemCompat.getActionView(item);
+        rightIcon = (ImageView) view.findViewById(R.id.imageView2);
+
+        return true;
     }
 }
